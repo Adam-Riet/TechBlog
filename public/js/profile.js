daad9fe3dd96a0749effc2b600ecbbe637952e85
riet <adamriet@gmail.com>
2023-06-20 13:13:56 -0600

    fixed new posts not saving contents

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,13 +1,13 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
+  const name = document.querySelector('#post-name').value.trim();
   // Remove needed_funding
-  const description = document.querySelector('#project-desc').value.trim();
+  const description = document.querySelector('#post-desc').value.trim();
 
   // Check for name and description only
   if (name && description) {
-    const response = await fetch(`/api/projects`, {
+    const response = await fetch(`/api/posts`, {
       method: 'POST',
       // Remove needed_funding from the request body
       body: JSON.stringify({ name, description }),
@@ -19,7 +19,7 @@ const newFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to create project');
+      alert('Failed to create post');
     }
   }
 };
@@ -28,22 +28,22 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/projects/${id}`, {
+    const response = await fetch(`/api/posts/${id}`, {
       method: 'DELETE',
     });
 
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to delete project');
+      alert('Failed to delete post');
     }
   }
 };
 
 document
-  .querySelector('.new-project-form')
+  .querySelector('.new-post-form')
   .addEventListener('submit', newFormHandler);
 
 document
-  .querySelector('.project-list')
+  .querySelector('.post-list')
   .addEventListener('click', delButtonHandler);
